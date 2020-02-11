fd0a2942b6c5b730e1b57e07b7f97444c30f54b8
lesreddy <les.reddy@gmail.com>
2020-02-11 12:20:29 +0000

    setup basic cart app structure

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as urls_products
+from cart import urls as urls_cart
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -28,5 +29,6 @@ urlpatterns = [
     url(r'^$', all_products, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT})  
 ]
