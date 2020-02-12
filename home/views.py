667dba309746d50fba8d3a5295340f1d40ab2042
lesreddy <les.reddy@gmail.com>
2020-02-12 11:36:25 +0000

    started styling products page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
-
-def index(request):
-    """A view that displays the index page"""
-    return render(request, "index.html")
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
--- products/views.py
+++ products/views.py
@@ -5,4 +5,3 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
-
