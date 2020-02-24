c8dba47159b22bfac686a4d40c618d6caff9a060
lesreddy <les.reddy@gmail.com>
2020-02-24 11:39:34 +0000

    added logic to chkout btn fixed order of ops

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,9 +134,8 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STRIPE_PUBLISHABLE = os.getenv('SRIPE_PUBLISHABLE')
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
-
 
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
