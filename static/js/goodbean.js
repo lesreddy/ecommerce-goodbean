16a24da801b10b28829ae3e05e17775b7a6decc9
lesreddy <les.reddy@gmail.com>
2020-03-05 12:09:37 +0000

    added js and html for google map in footer

--- static/js/goodbean.js
+++ static/js/goodbean.js
@@ -1,3 +1,24 @@
 // Make messages close after 4 seconds
 
 setTimeout(function(){document.getElementById('messages').style.display='none';},4000);
+
+// Goodbean location in Footer
+
+function initMap() {
+            {
+                var goodbean = {
+                    lat: 52.664292,
+                    lng: -8.627918
+                };
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: goodbean
+                });
+                var marker = new google.maps.Marker({
+                    position: goodbean,
+                    map: map
+                });
+
+            }
+        }
