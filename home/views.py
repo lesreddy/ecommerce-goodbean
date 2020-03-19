44109ae81c91ac3261e6d995d94899210fa7f379
lesreddy <les.reddy@gmail.com>
2020-03-19 20:11:27 +0000

    setup model views and form for contact page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, reverse
+from .forms import ContactForm
 
-
-# Create your views here.
 def home(request):
     """A view that renders the home page"""
     return render(request, "home.html")
@@ -13,3 +12,20 @@ def contact(request):
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+def contact(request):
+    template = "contact.html"
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+    
+    else:
+        form = ContactForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
