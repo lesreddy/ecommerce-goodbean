c3e82a77ee8538d249e476ee62cf3c10fdb7b16a
lesreddy <les.reddy@gmail.com>
2020-04-02 08:06:11 +0000

    setup review models for products

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=500)
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.username
