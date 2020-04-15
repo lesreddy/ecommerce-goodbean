e96313ac47757a0dc36c5b8be77fa23239a21579
lesreddy <les.reddy@gmail.com>
2020-04-15 22:21:35 +0000

    fixed js error

--- static/js/goodbean.js
+++ static/js/goodbean.js
@@ -1,7 +1,7 @@
 // Make messages close after 4 seconds
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  setTimeout(function(){document.getElementById('messages').style.display='none';},4000);
+  setTimeout(function(){document.getElementById('messages').style.display='none';} ,4000);
 });
 
 
