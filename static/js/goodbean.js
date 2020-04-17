f5c33252eb79c37368dbc6c0f9d72e6ddf9adc6a
lesreddy <les.reddy@gmail.com>
2020-04-17 08:31:31 +0000

    fixed js error

--- static/js/goodbean.js
+++ static/js/goodbean.js
@@ -1,25 +1,25 @@
-// Make messages close after 4 seconds
-
-setTimeout(function(){document.getElementById('messages').style.display='none';} ,4000);
-
-
-// Goodbean location in Footer
-
-function initMap() {
-            {
-                const goodbean = {
-                    lat: 52.664292,
-                    lng: -8.627918
-                };
-
-                const map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
-                    center: goodbean
-                });
-                const marker = new google.maps.Marker({
-                    position: goodbean,
-                    map: map
-                });
-
-            }
-        }
+$(document).ready(function () {
+  setTimeout(function () {
+    document.getElementById('messages').style.display = 'none';
+  }, 4000);
+  
+/**
+ * Goodbean location in Footer
+ */
+  function initMap() {
+    {
+      const goodbean = {
+        lat: 52.664292,
+        lng: -8.627918,
+      };
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: goodbean,
+      });
+      const marker = new google.maps.Marker({
+        position: goodbean,
+        map: map,
+      });
+    }
+  }
+});
